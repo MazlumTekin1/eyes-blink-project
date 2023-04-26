3a6e950bf493356ec8630fae06a77c3d33e43c5b
MazlumTekin <mazlum.tekin@diatics.com>
2023-04-26 22:12:01 +0300

    project_added

---
+++ router.py
@@ -0,0 +1,13 @@
+
+
+def LeftEyeBlinked():
+    print("Left Eye Blinked")
+
+def RightEyeBlinked():
+    print("Righ Eye Blinked")
+
+def BothEyesBlinkedFor3Seconds():
+    print("Both Eyes Blinked For 3 Seconds")
+
+def BothEyesBlinkedFor1Seconds():
+    print("Both Eyes Blinked For 1 Second")
